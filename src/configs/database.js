d55da1df48cd1eaa7e2b7ee9c34e8f37b20876dc
Artur Silva Vaz <tutuvaz89@gmail.com>
2026-04-03 22:53:39 -0300

    feat: configure database connection using environment variables

---
+++ src/configs/database.js
@@ -0,0 +1,28 @@
+'use strict'; 
+
+const mysql = require('mysql2')
+require('dotenv').config();
+
+const config_db = mysql.createPool({
+ host : process.env.DB_HOST,
+ port : process.env.DB_PORT,
+ user : process.env.DB_USER,
+ password : process.env.DB_PASSWORD, 
+ database : process.env.DATA_BASE_NAME,
+});  
+
+
+config_db.getConnection((err,connection) => {
+    if (err) {
+        console.error('Erro ao conectar no banco de dados', err)
+    } else {
+        console.log('Conectado ao banco com sucesso!')
+        connection.release();
+    }
+}
+
+
+
+)
+
+module.exports = config_db; 
