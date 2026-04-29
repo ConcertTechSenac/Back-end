ed070db80b162b7158b355a6f5cbc2d19b1b9412
Gabriel Magalhães <magalhaesg139@gmail.com>
2026-04-28 21:55:36 -0300

    Ligação
    Feito a conexão do login, cadastro, dados do usuario e a logica do usuario logado.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // 1. Importe o pacote cors
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+// 2. Configure o CORS para aceitar conexões do seu Front-end
+app.use(cors()); 
 
 app.use(express.json()); 
 
@@ -15,7 +19,7 @@ app.get('/', (req, res) => {
 // Rotas de autenticação
 app.use('/api/auth', authRoutes);
 
-// Forçando o Node a escutar em todas as interfaces de rede (IPv4 e IPv6)
+// Forçando o Node a escutar em todas as interfaces de rede
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
